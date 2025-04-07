32d4dcd390c05f93041ac2a4c81b1d490c234f26
marinalp04 <arriagamarina4@gmail.com>
2025-04-07 12:15:34 +0200

    feat: añadir datos de ejemplo para CategoriaFixtures

--- src/DataFixtures/CategoriaFixtures.php
+++ src/DataFixtures/CategoriaFixtures.php
@@ -2,6 +2,7 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Categoria;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
@@ -9,9 +10,21 @@ class CategoriaFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        
+        $categoria1 = new Categoria();
+        $categoria1->setNombre('Platos vegetarianos');
+        $manager->persist($categoria1);
+
+        $categoria2 = new Categoria();
+        $categoria2->setNombre('Platos proteicos');
+        $manager->persist($categoria2);
+
+        
 
         $manager->flush();
+
+        $this->addReference('categoria1', $categoria1);
+        $this->addReference('categoria2', $categoria2);
+       
     }
 }
