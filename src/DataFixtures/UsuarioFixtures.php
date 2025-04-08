4c230c80efc30c7ebf914cc848e7d5387f1bcd9c
marinalp04 <arriagamarina4@gmail.com>
2025-04-08 18:35:53 +0200

    feat: añadir datos de ejemplo para UsuarioFixtures

--- src/DataFixtures/UsuarioFixtures.php
+++ src/DataFixtures/UsuarioFixtures.php
@@ -2,16 +2,34 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Usuario;
 use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 use Doctrine\Persistence\ObjectManager;
 
-class UsuarioFixtures extends Fixture
+class UsuarioFixtures extends Fixture 
 {
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+       
+        $usuario1 = new Usuario();
+        $usuario1->setNombre('Marina');
+        $usuario1->setApellidos('Lopez Arriaga');
+        $usuario1->setEmail('mloparr@gmail.com');
+        $usuario1->setContrasena('1234');
+        $usuario1->setDireccion('Calle de la Paz, 123');
+        $usuario1->setTelefono('123456789');
+        $usuario1->setRol('0');
+        $manager->persist($usuario1);
+
+        
 
         $manager->flush();
+
+        $this->addReference('usuario1', $usuario1);
+       
     }
+
+    
+    
 }
