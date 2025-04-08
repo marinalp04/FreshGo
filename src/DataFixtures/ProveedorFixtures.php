494a639cf70385c72966381da7782cb4637961d6
marinalp04 <arriagamarina4@gmail.com>
2025-04-08 18:38:27 +0200

    feat: añadir datos de ejemplo para ProveedorFixtures

--- src/DataFixtures/ProveedorFixtures.php
+++ src/DataFixtures/ProveedorFixtures.php
@@ -2,6 +2,7 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Proveedor;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
@@ -9,9 +10,18 @@ class ProveedorFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        
+        $proveedor1 = new Proveedor();
+        $proveedor1->setNombre('Juan Frutas SL');
+        $proveedor1->setEmail('juan@gmail.com');
+        $proveedor1->setDireccion('Calle de la Paz, 123');
+        $proveedor1->setTelefono('123456789');
+        $manager->persist($proveedor1);
+
+        
 
         $manager->flush();
+
+        $this->addReference('proveedor1', $proveedor1);
     }
 }
