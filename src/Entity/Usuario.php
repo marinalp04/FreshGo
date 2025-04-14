775d0041fb6ce15ad90f786d582b7a9773554710
marinalp04 <arriagamarina4@gmail.com>
2025-04-14 14:04:03 +0200

    feat: login y logout funcionando con correo visible en navbar

--- src/Entity/Usuario.php
+++ src/Entity/Usuario.php
@@ -7,9 +7,10 @@ use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 
 #[ORM\Entity(repositoryClass: UsuarioRepository::class)]
-class Usuario implements UserInterface
+class Usuario implements UserInterface, PasswordAuthenticatedUserInterface
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
