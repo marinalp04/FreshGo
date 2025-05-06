6916da84a7441920a865cec29f8d2da5176915c0
marinalp04 <arriagamarina4@gmail.com>
2025-05-06 19:10:36 +0200

    feat(admin): configurar acceso al panel de administración con EasyAdmin

--- config/bundles.php
+++ config/bundles.php
@@ -12,4 +12,6 @@ return [
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
+    Symfony\UX\TwigComponent\TwigComponentBundle::class => ['all' => true],
+    EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
 ];
--- src/DataFixtures/UsuarioFixtures.php
+++ src/DataFixtures/UsuarioFixtures.php
@@ -6,9 +6,11 @@ use App\Entity\Usuario;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class UsuarioFixtures extends Fixture 
 {
+    public function __construct(private UserPasswordHasherInterface $passwordHasher) {}
     public function load(ObjectManager $manager): void
     {
        
@@ -16,11 +18,25 @@ class UsuarioFixtures extends Fixture
         $usuario1->setNombre('Marina');
         $usuario1->setApellidos('Lopez Arriaga');
         $usuario1->setEmail('mloparr@gmail.com');
-        $usuario1->setPassword('1234');
+        $usuario1->setPassword(
+            $this->passwordHasher->hashPassword($usuario1, '1234')
+        );
         $usuario1->setDireccion('Calle de la Paz, 123');
         $usuario1->setTelefono('123456789');
-        $usuario1->setRol('0');
+        $usuario1->setRoles(['ROLE_ADMIN']);
         $manager->persist($usuario1);
+
+        $usuario2 = new Usuario();
+        $usuario2->setNombre('Jose');
+        $usuario2->setApellidos('Perez');
+        $usuario2->setEmail('jose@gmail.com');
+        $usuario2->setPassword(
+            $this->passwordHasher->hashPassword($usuario2, '1234')
+        );
+        $usuario2->setDireccion('Calle de la Paz, 223');
+        $usuario2->setTelefono('223456789');
+        $usuario2->setRoles(['ROLE_USER']);
+        $manager->persist($usuario2);
 
         
 
