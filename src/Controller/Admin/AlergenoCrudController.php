cf65d232fecbfe31d72e0dba206d1df3510a3a97
marinalp04 <arriagamarina4@gmail.com>
2025-06-01 16:52:45 +0200

    feat: restringir acciones en CRUD de alérgenos para administradores de solo lectura

--- src/Controller/Admin/AlergenoCrudController.php
+++ src/Controller/Admin/AlergenoCrudController.php
@@ -3,16 +3,24 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Alergeno;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use Symfony\Bundle\SecurityBundle\Security;
 
 class AlergenoCrudController extends AbstractCrudController
 {
     public static function getEntityFqcn(): string
     {
         return Alergeno::class;
+    }
+
+    public function __construct(private Security $security) {
+        
     }
 
     
@@ -23,5 +31,17 @@ class AlergenoCrudController extends AbstractCrudController
             TextField::new('nombre'),
         ];
     }
+
+    public function configureActions(Actions $actions): Actions
+    {
+        if ($this->security->isGranted('ROLE_ADMIN_READONLY') && 
+            !$this->security->isGranted('ROLE_ADMIN')) {
+            
+            return $actions
+                ->disable(Action::NEW, Action::EDIT, Action::DELETE);
+        }
+        return $actions;
+    }
+
     
 }
